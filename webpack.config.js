0520ffa32d53afb787a9071494bca75a45138672
Siyath Kasali <jsiyath@gmail.com>
2020-07-27 23:28:21 +0530

    Added webpack config and babel

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: ['@babel/polyfill', './src/index.js']
+  },
+  output: {
+    filename: 'index_bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module : {
+    rules: [{
+      // A regex that looks at all Javascript files
+      test: /\.js?$/,
+      exclude : /node_modules/,
+      loader: 'babel-loader',
+      //this is where we define our presets
+      query: {
+        presets:['@babel/preset-env']
+      }
+    }]
+  },
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, 'dist'),
+    inline: true,
+    hot: true
+  }
+};
